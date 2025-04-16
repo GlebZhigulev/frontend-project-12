939b065fc35df6c67f6cbb3eeb1a1a5344a3cd72
Zhigylev <glebzhigulevv@gmail.com>
2025-04-17 00:40:23 +0300

    change routing

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { removeToken } from './slices/authSlice';
 import Chat from './Chat.jsx';
 import Login from './Login.jsx';
@@ -9,26 +9,31 @@ import Signup from './Signup.jsx';
 import NotFound from './NotFound.jsx';
 import Header from './Header.jsx';
 
-
 const App = () => {
   const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (!token) {
       dispatch(removeToken());
     }
   }, [dispatch]);
+
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Chat />} />
+        <Route
+          path="/"
+          element={token ? <Chat /> : <Navigate to="/login" replace />}
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
-  ); 
+  );
 };
 
 export default App;
